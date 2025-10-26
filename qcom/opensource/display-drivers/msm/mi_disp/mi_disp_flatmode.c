SleepForge/kernel_xiaomi_sm8635-modules

display-drivers: mi_disp: Pass panel with dsi_panel_parse_cmd_sets_sub
 * needed since https://git.codelinaro.org/clo/la/platform/vendor/opensource/display-drivers/-/commit/710a420505193ff1a46fcbd0a0ba6d795bb00d38

Change-Id: Id65a0705644d082e600e5b161c157872ea0c940c

qcom/opensource/display-drivers/msm/mi_disp/mi_disp_flatmode.c
@@ -62,12 +62,12 @@ static int mi_dsi_panel_parse_flatmode_configs(
 	if (!config->flatmode_check_enabled)
 		goto error;
 
-	dsi_panel_parse_cmd_sets_sub(&config->offset_cmd,
+	dsi_panel_parse_cmd_sets_sub(panel, &config->offset_cmd,
 				DSI_CMD_SET_MI_FLATMODE_STATUS_OFFSET, utils);
 	if (!config->offset_cmd.count)
 		DISP_INFO("flat mode status offset command parsing failed\n");
 
-	dsi_panel_parse_cmd_sets_sub(&config->status_cmd,
+	dsi_panel_parse_cmd_sets_sub(panel ,&config->status_cmd,
 				DSI_CMD_SET_MI_FLATMODE_STATUS, utils);
 	if (!config->status_cmd.count) {
 		DISP_ERROR("flat mode status command parsing failed\n");
@@ -126,7 +126,7 @@ static int mi_dsi_panel_parse_flatmode_configs(
 		goto error5;
 	}
 
-	dsi_panel_parse_cmd_sets_sub(&config->offset_end_cmd,
+	dsi_panel_parse_cmd_sets_sub(panel ,&config->offset_end_cmd,
 				DSI_CMD_SET_MI_FLATMODE_STATUS_OFFSET_END, utils);
 	if (!config->offset_end_cmd.count) {
 		DISP_INFO("flat mode status end command parsing failed\n");

PixelOS-Devices/android_kernel_xiaomi_sm8635-modules

qcom/opensource/display-drivers/msm/mi_disp/mi_disp_flatmode.c
@@ -62,12 +62,12 @@ mi_dsi_panel_parse_flatmode_configs(struct dsi_panel *panel,
   if (!config->flatmode_check_enabled)
     goto error;
 
-  dsi_panel_parse_cmd_sets_sub(&config->offset_cmd,
+  dsi_panel_parse_cmd_sets_sub(panel, &config->offset_cmd,
                                DSI_CMD_SET_MI_FLATMODE_STATUS_OFFSET, utils);
   if (!config->offset_cmd.count)
     DISP_INFO("flat mode status offset command parsing failed\n");
 
-  dsi_panel_parse_cmd_sets_sub(&config->status_cmd,
+  dsi_panel_parse_cmd_sets_sub(panel, &config->status_cmd,
                                DSI_CMD_SET_MI_FLATMODE_STATUS, utils);
   if (!config->status_cmd.count) {
     DISP_ERROR("flat mode status command parsing failed\n");
@@ -125,7 +125,7 @@ mi_dsi_panel_parse_flatmode_configs(struct dsi_panel *panel,
     goto error5;
   }
 
-  dsi_panel_parse_cmd_sets_sub(&config->offset_end_cmd,
+  dsi_panel_parse_cmd_sets_sub(panel, &config->offset_end_cmd,
                                DSI_CMD_SET_MI_FLATMODE_STATUS_OFFSET_END,
                                utils);
   if (!config->offset_end_cmd.count) {